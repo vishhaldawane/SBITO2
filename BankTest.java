8d81278c3b75030e400ca31473916e91f2a9ab49
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-03-22 15:24:48 +0530

    local date is added, age is calculated

--- BankTest.java
+++ BankTest.java
@@ -1,45 +1,35 @@
+import java.time.LocalDate;
 
 public class BankTest {
 
 	public static void main(String[] args) {
 
+		
+		/*LocalDate   sampleDate = LocalDate.of(2022, 3, 25);
+		
+		System.out.println("date is "+sampleDate);
+		System.out.println("year   "+sampleDate.getYear());
+		System.out.println("Month  "+sampleDate.getMonth());
+		System.out.println("Day    "+sampleDate.getDayOfMonth());
+		*/
+		
+		
+		
 		BankAccount baObj1 = new BankAccount();
-		
 		BankAccount baObj2 = new BankAccount();
-		baObj2.setBankAccount(102, "Mayur Wankhede", 95000.0f,20);
-		
 		BankAccount baObj3 = new BankAccount();
 		BankAccount baObj4 = new BankAccount();
 		BankAccount baObj5 = new BankAccount();
 		
-		baObj1.setBankAccount(101, "Shashi Bamne", 90000.0f,23);
+		baObj1.setBankAccount(101, "Shashi Bamne", 90000.0f, LocalDate.of(2019, 1, 10), LocalDate.of(2000, 6, 10));
+		baObj1.printBankAccount();
+
+		baObj1.changeAccountHolderName("SHASHI BAMNE");
+
 		baObj1.printBankAccount();
 		
-		float updatedBalance = baObj1.withdraw(10000);
-		System.out.println("Updated balance is "+updatedBalance);
 		
-		
-		updatedBalance = baObj1.withdraw(5000);
-		System.out.println("Updated balance is "+updatedBalance);
-		
-		updatedBalance = baObj1.withdraw(7000);
-		System.out.println("Updated balance is "+updatedBalance);
-		
-		
-		updatedBalance = baObj1.deposit(75000);
-		System.out.println("Updated balance is "+updatedBalance);
-		
-		
-		float si = baObj1.calculateSimpleInterest();
-		System.out.println("Simple interest is "+si);
-		
-		
-		float si1 = baObj2.calculateSimpleInterest();
-		System.out.println("Simple interest is "+si1);
-		
-		
-		
-		/*
+		/*baObj2.setBankAccount(102, "Mayur Wankhede", 95000.0f,20);
 		baObj3.setBankAccount(103, "Jyotsna Kasukurthi", 98000.0f,19);
 		baObj4.setBankAccount(104, "Poornima Athre", 99000.0f,18);
 		baObj5.setBankAccount(105, "Sagar Burade", 190000.0f,21);
@@ -57,31 +47,41 @@ public class BankTest {
 
 //isA  hasA  usesA  producesA
 
+
+// ACCESSOR  VS MUTATOR
+
 class BankAccount
 {
 	//DATA-MEMBER / fields  SECTION
 	int accountNumber;
 	String accountHolderName;
 	float accountBalance;
+	LocalDate accountOpeningDate;
+	LocalDate accountHolderBirthdate;
 	int age;
-	
 	
 	//MEMBER-FUNCTIONS / methods SECTION
 	
-	void setBankAccount(int x, String y, float z, int a) {
+	void setBankAccount(int x, String y, float z, LocalDate aod, LocalDate dob) {
 		System.out.println("Setting the bank account details....");
 		accountNumber = x;
 		accountHolderName = y;
 		accountBalance= z;
-		age = a;
+		accountOpeningDate = aod;
+		accountHolderBirthdate = dob;
+		
+		LocalDate todaysDate = LocalDate.now();
+		
+		age = todaysDate.getYear() - dob.getYear();
 	}
 	
 	void printBankAccount() {
 		System.out.println("Bank Account Number  : "+accountNumber);
 		System.out.println("Bank Holder Name     : "+accountHolderName);
 		System.out.println("Bank Account Balance : Rs."+accountBalance+"/-");
+		System.out.println("Account Opening Date : "+accountOpeningDate);
+		System.out.println("Account Holder DOB   : "+accountHolderBirthdate);
 		System.out.println("Bank Holder's Age    : "+age+" years");
-		
 		System.out.println("---------------------");
 	}
 	
@@ -102,6 +102,11 @@ class BankAccount
 		return si;
 	}
 
+	void changeAccountHolderName(String newName) {
+		System.out.println("changing the account holder's name to "+newName);
+		accountHolderName = newName;
+	}
+	
 }
 
 
